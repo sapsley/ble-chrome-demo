b9c7921c591ac19376821e8c722cba9262ddba47
sapsley <spencer.apsley@gmail.com>
2016-09-09 09:38:17 -0600

    Fixed js

--- noke-chrome-demo.js
+++ noke-chrome-demo.js
@@ -1,16 +1,19 @@
 function onButtonClick() {
-  
+ // Validate services UUID entered by user first.
+  let services = document.querySelector('#optionalServices').value.split(/, ?/)
+    .map(s => s.startsWith('0x') ? parseInt(s) : s)
+    .filter(s => s && BluetoothUUID.getService);
 
   log('Requesting any Bluetooth Device...');
   navigator.bluetooth.requestDevice(
-    {filters: anyDevice()})
+    {filters: anyDevice(), optionalServices: services})
   .then(device => {
     log('> Name:             ' + device.name);
     log('> Allowed Services: ' + device.uuids.join('\n' + ' '.repeat(20)));
     log('Connecting to GATT Server...');
     return device.gatt.connect();
   })
-  .then(server => {
+    .then(server => {
     // Note that we could also get all services that match a specific UUID by
     // passing it to getPrimaryServices().
     log('Getting Services...');
